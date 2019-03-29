a5425eefae3de11ac6c4f35b4369f1a61ea6abd4
June <yula.qian@gmail.com>
2019-03-29 11:03:27 +0800

    404 page

--- src/view/404.vue
+++ src/view/404.vue
@@ -1,10 +1,15 @@
 <template>
-    <div>
-        404
+    <div class="warp">
+        <img src="../assets/pic/logo_blue.png" width="100%" alt="404">
     </div>
 </template>
 <script>
 export default {
-    name: '404',
+    name: 'miss-page',
 }
 </script>
+<style type="text/css" scoped>
+	.warp{
+		margin-top: 20vh;
+	}
+</style>
